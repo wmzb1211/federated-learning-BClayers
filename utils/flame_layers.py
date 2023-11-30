bfde42a1d4ee0f945653265977bffa4a63feea83
HenryZhu <2468877985@qq.com>
2023-11-30 23:06:59 +0800

    small fix 5.1

--- utils/flame_layers.py
+++ utils/flame_layers.py
@@ -13,6 +13,7 @@ def flame_layers(w_locals_k: list, w_glob_k: dict, args):
     '''
     # 1.cosine similarity hdbscan clustering
     num_clients = len(w_locals_k)
+    # print("num_clients: ", num_clients)
     clusterer = hdbscan.HDBSCAN(metric="cosine", algorithm='generic', min_cluster_size=num_clients // 2 + 1, min_samples=1, allow_single_cluster=True)
     # w_locals_k_total = torch.stack([torch.stack([torch.flatten(w_locals_k[i])]) for i in range(num_clients)])
     # w_locals_k_total_temp = w_locals_k_total.cpu().numpy()
@@ -21,6 +22,7 @@ def flame_layers(w_locals_k: list, w_glob_k: dict, args):
     # w_locals_k_total_temp = np.array(w_locals_k_total_temp, dtype=np.double)
     w_locals_k_total = torch.stack([w_locals_k[i].view(-1) for i in range(num_clients)])
     w_locals_k_total_temp = w_locals_k_total.cpu().double()
+    print("w_locals_k_total_temp.shape: ", w_locals_k_total_temp.shape)
     # w_locals_k_total_temp = np.array(w_locals_k_total_temp, dtype=np.double)
     clusterer.fit(w_locals_k_total_temp)
     benign_cluster = []
