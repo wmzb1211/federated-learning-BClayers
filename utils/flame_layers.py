e0ffef5d052def3d34a1407280467a0a7d60f5e1
HenryZhu <2468877985@qq.com>
2023-11-30 23:03:18 +0800

    small fix 5.1

--- utils/flame_layers.py
+++ utils/flame_layers.py
@@ -20,9 +20,7 @@ def flame_layers(w_locals_k: list, w_glob_k: dict, args):
     # w_locals_k_total_temp = w_locals_k_total_temp.reshape(w_locals_k_total_temp.shape[0], -1)
     # w_locals_k_total_temp = np.array(w_locals_k_total_temp, dtype=np.double)
     w_locals_k_total = torch.stack([w_locals_k[i].view(-1) for i in range(num_clients)])
-    w_locals_k_total_temp = w_locals_k_total.cpu()
-    # double
-    w_locals_k_total_temp = np.array(w_locals_k_total_temp, dtype=np.double)
+    w_locals_k_total_temp = w_locals_k_total.cpu().double()
     # w_locals_k_total_temp = np.array(w_locals_k_total_temp, dtype=np.double)
     clusterer.fit(w_locals_k_total_temp)
     benign_cluster = []
