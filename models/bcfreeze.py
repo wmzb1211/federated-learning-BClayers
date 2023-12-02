8c9d0435e497cb19dde34f5356e259c73033163c
HenryZhu <2468877985@qq.com>
2023-12-02 22:44:59 +0800

    a test in 2023.12.2 2.5

--- models/bcfreeze.py
+++ models/bcfreeze.py
@@ -13,15 +13,16 @@ from utils.flame_layers import flame_layers
 #         for w_local in w_locals_k:
 #             value += w_local[key] / len(w_locals_k)
 #     return net_glob_k
-def fedavg_layers(w_locals_k, net_glob_k):
+def fedavg_layers(w_locals_k):
     '''
     :param w_locals: list of state_dict, 代表各个client的本地模型参数
     :param net_glob: model, 代表global model
     :return:
     '''
-    value = net_glob_k
-    for w_local in w_locals_k:
-        value += w_local / len(w_locals_k)
+    value = w_locals_k[0]
+    for w_local in w_locals_k[1:]:
+        value += w_local
+    value /= len(w_locals_k)
     return value
 def bcfreeze(w_locals, net_glob, args):
     '''
@@ -30,9 +31,9 @@ def bcfreeze(w_locals, net_glob, args):
     :return:
     '''
     w_glob = net_glob.state_dict()
-    for i in range(len(w_locals)):
-        for key, value in w_locals[i].items():
-            w_locals[i][key] = value - w_glob[key]
+    # for i in range(len(w_locals)):
+    #     for key, value in w_locals[i].items():
+    #         w_locals[i][key] = value - w_glob[key]
     print("num of len w_locals: ", len(w_locals))
     for key in w_glob.keys():
         w_locals_k = [w_local[key].cpu() for w_local in w_locals]
@@ -45,5 +46,5 @@ def bcfreeze(w_locals, net_glob, args):
 
         else:
             print("Average layer: ", key)
-            w_glob[key] = fedavg_layers(w_locals_k, w_glob_k)
+            w_glob[key] = fedavg_layers(w_locals_k)
     return w_glob
